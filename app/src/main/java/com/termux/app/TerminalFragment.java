86a52280461226efeb6eb16a38ad3c213e28e6fa
Rick000000007 <119560084+Rick000000007@users.noreply.github.com>
2026-02-26 02:08:17 +0530

    Add TerminalFragment to handle terminal view

---
+++ app/src/main/java/com/termux/app/TerminalFragment.java
@@ -0,0 +1,24 @@
+package com.termux.app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.termux.view.TerminalView;
+
+public class TerminalFragment extends Fragment {
+
+    private TerminalView terminalView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        terminalView = new TerminalView(requireContext());
+        return terminalView;
+    }
+}
